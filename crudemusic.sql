44434888e320ed8f5b4c3b83ce6a7993f95ec5cb
Mar10s <115137230+Mar10s@users.noreply.github.com>
2022-11-03 12:34:24 +0200

    Update crudemusic.sql

--- crudemusic.sql
+++ crudemusic.sql
@@ -7,6 +7,13 @@ select * from songs;
 select bands.name,albums.name from bands join albums on Bands.idBand = albums.Bands_idBand where bands.name="Metallica";
 select songs.name,albums.name from albums join songs on albums.idAlbums = songs.Albums_idAlbums where albums.Bands_idBand=5;
 
+select * from
+genre join bands join albums join liveappearances join songs
+on genre.id = bands.Genre_id 
+and bands.idband=liveappearances.bands_idband
+and bands.idband=albums.Bands_idBand
+and albums.idAlbums = songs.Albums_idAlbums;
+
 /*Transaction is a way to group statements,check what the output will be before commiting(especially when working with others in the same database) and also check for errors in your code and correct them*/
 START TRANSACTION;
 insert into genre(Type,Region,Year) /*Here we see what the transaction will output if we decide to commit  */
